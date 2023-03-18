2f9dc24daa6602bab3f6745225f723c44e7b37db
Alexey <alexei20293@yahoo.com>
2023-03-18 12:39:07 +0200

    add(helpers): asyncWrapper, customError, handleMongooseError

---
+++ helpers/index.js
@@ -0,0 +1,9 @@
+const customError = require("./customError");
+const handleMongooseError = require("./handleMongooseError");
+const asyncWrapper = require("./asyncWrapper");
+
+module.exports = {
+  customError,
+  handleMongooseError,
+  asyncWrapper,
+};
